e638b5ef435925a83d7fa238929d6f90eda9c468
Nathan Dang <ndang5@huskers.unl.edu>
2025-07-24 15:37:03 -0500

    Add user input functionality for QR code generation

--- qr_code_generator.py
+++ qr_code_generator.py
@@ -1,2 +1,9 @@
 import qrcode
 
+#take input from user and ask to submit a link
+
+user_input = input("Enter text or link: ")
+
+img = qrcode.make(user_input)
+type(img)  # qrcode.image.pil.PilImage
+img.save("some_file.png")
