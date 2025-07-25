e6ba85613d5b8a90f2b82f557a74b3a970bd410b
Nathan Dang <ndang5@huskers.unl.edu>
2025-07-25 16:00:23 -0500

    Refactor input handling for clarity and consistency

--- qr_code_generator.py
+++ qr_code_generator.py
@@ -2,13 +2,12 @@ import qrcode
 
 # Take input from user and ask to submit a link
 # Then remove whitespace from string data
-data = str.strip(input("Enter text or URL: "))
-filename = input("Enter the filename: ")
-
-
+data = input("Enter text or URL: ").strip()
+# Asks user for what they want to name the file
+filename = input("Enter the filename: ").strip()
 
 # Create QR Code using data
 img = qrcode.make(data)
-type(img)  # qrcode.image.pil.PilImage
+
 # Save file as filename entered
 img.save(f"{filename}.png")
